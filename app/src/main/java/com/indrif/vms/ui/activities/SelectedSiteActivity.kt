cf95e3bfe2b322927fba47e19c80d8b8d1061821
Akhilesh <dpreet941@gmail.com>
2019-07-14 22:59:05 +0530

    Site Selection Ui

---
+++ app/src/main/java/com/indrif/vms/ui/activities/SelectedSiteActivity.kt
@@ -0,0 +1,18 @@
+package com.indrif.vms.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.indrif.vms.R
+import com.indrif.vms.core.BaseActivty
+
+class SelectedSiteActivity : BaseActivty() {
+    override fun onClick(v: View) {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selected_site)
+    }
+}
